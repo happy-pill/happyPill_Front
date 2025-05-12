4a857b12865cb426ad73fae2fedffbdd9bdb491d
yunzzng <leejiyun0114@naver.com>
2025-05-12 11:05:17 +0900

    Feat: 라우터 초기 설정

---
+++ src/router.tsx
@@ -0,0 +1,24 @@
+// router.tsx
+import { createBrowserRouter } from 'react-router-dom';
+import { Layout, ErrorPage, LoginPage, MyPage, Mainpage } from './utils';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Mainpage /> },
+      // 헤더/푸터가 있는 페이지들
+      { path: 'mypage', element: <MyPage /> },
+
+      {}
+    ],
+  },
+  {
+    path: '/login',
+    element: <LoginPage />, // 헤더/푸터 없는 페이지
+  },
+]);
+
+export default router;
