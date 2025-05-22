38640fddb0813b37251d50f8171e6b01b043c6a8
minseung-gang <strause1@nate.com>
2025-05-22 16:17:43 +0900

    feat: 모달 활성 시 배경 스크롤 방지 기능 추가

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -1,7 +1,20 @@
+import { useEffect } from 'react'
 import useModalStore, { ModalConfig } from '../store/modal'
 
 const useModal = () => {
   const { modals, openModal, closeModal } = useModalStore()
+
+  useEffect(() => {
+    if (modals.length === 0) {
+      document.body.style.removeProperty('overflow')
+    } else {
+      document.body.style.overflowY = 'hidden'
+    }
+
+    return () => {
+      document.body.style.removeProperty('overflow')
+    }
+  }, [modals.length])
 
   const handleOpenModal = (modal: ModalConfig) => {
     openModal(modal)
