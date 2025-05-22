9fb93373ca7d4824881ca6f74636fd90a54aa99f
yunzzng <leejiyun0114@naver.com>
2025-05-22 15:39:36 +0900

    Fix: 메인페이지 라우터 오타 수정

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,6 @@
 // router.tsx
 import { createBrowserRouter } from 'react-router-dom'
-import { Layout, ErrorPage, LoginPage, MyPage, Mainpage, RefundPage, AboutPage } from './utils'
+import { Layout, ErrorPage, LoginPage, MyPage, RefundPage, AboutPage, MainPage } from './utils'
 import PurchasedItemsListPage from './pages/admin/PurchasedItemsListPage'
 
 const router = createBrowserRouter([
@@ -9,7 +9,7 @@ const router = createBrowserRouter([
     element: <Layout />,
     errorElement: <ErrorPage />,
     children: [
-      { index: true, element: <Mainpage /> },
+      { index: true, element: <MainPage /> },
       // 헤더/푸터가 있는 페이지들
       { path: 'mypage', element: <MyPage /> },
       { path: 'refund', element: <RefundPage /> },
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
 export { default as Layout } from "../components/Layout";
-export { default as Mainpage } from "../pages/MainPage";
+export { default as MainPage } from "../pages/MainPage";
 export { default as ErrorPage } from  "../pages/ErrorPage";
 export { default as LoginPage } from  "../pages/user/LoginPage";
 export { default as MyPage } from  "../pages/user/MyPage";
