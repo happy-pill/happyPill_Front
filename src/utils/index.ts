5d7fa27c367b1b7d64d02b56d22f1297db1b7965
minseung-gang <strause1@nate.com>
2025-05-23 21:59:22 +0900

    refector: maiaPage 경로 수정

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,7 @@
-export { default as Layout } from "../components/Layout";
-export { default as Mainpage } from "../pages/MainPage";
-export { default as ErrorPage } from  "../pages/ErrorPage";
-export { default as LoginPage } from  "../pages/user/LoginPage";
-export { default as MyPage } from  "../pages/user/MyPage";
+export { default as Layout } from '../components/Layout'
+export { default as MainPage } from '../pages/product/MainPage'
+export { default as ErrorPage } from '../pages/ErrorPage'
+export { default as LoginPage } from '../pages/user/LoginPage'
+export { default as MyPage } from '../pages/user/MyPage'
+export { default as RefundPage } from '../pages/RefundPage'
+export { default as AboutPage } from '../pages/AboutPage'
