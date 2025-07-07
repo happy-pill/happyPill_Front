daaa552fd7e7ad557c367f7e5d4e72b784be3b10
JiEun <109754988+jieun419@users.noreply.github.com>
2025-07-07 14:49:42 +0900

    Chore: LayoutContainer 삭제 됨에 따라 불필요한 코드 삭제

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,5 @@
 import { Outlet } from 'react-router-dom';
 
-import LayoutContainer from './container/LayoutContainer';
 import Footer from './Footer';
 import Header from './Header';
 import GlobalContainer from './modal/GlobalContainer';
