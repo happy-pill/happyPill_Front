7e8eb50f477d7b65945794a1d560223b6269887c
Choi Jieun <crg1050@gmail.com>
2025-07-11 18:17:24 +0900

    Chore: props로 관리되는 LayoutContainer cn() 함수 추가

--- src/components/container/LayoutContainer.tsx
+++ src/components/container/LayoutContainer.tsx
@@ -1,12 +1,15 @@
+import { cn } from '@/utils/classNames';
+
 interface LayoutContainerProps {
   children: React.ReactNode;
   className: string;
+  // isHeader: boolean;
   px?: 'px-md' | 'px-0';
 }
 
 const LayoutContainer = ({ children, className, px = 'px-md' }: LayoutContainerProps) => {
   return (
-    <main className={`mx-auto w-full max-w-(--max-width) flex-1 ${px} ${className}`}>
+    <main className={cn(`max-width-container mx-auto mt-[100px] w-full flex-1 ${px}`, className)}>
       {children}
     </main>
   );
