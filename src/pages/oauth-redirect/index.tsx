bc2af40b0109cfc80991a2f3457cfaeb85d2cb37
Choi Jieun <crg1050@gmail.com>
2025-07-13 19:48:02 +0900

    Feat: OauthRedirect 시 리프레쉬 토큰 쿠키에 저장

--- src/pages/oauth-redirect/index.tsx
+++ src/pages/oauth-redirect/index.tsx
@@ -1,22 +1,23 @@
 import { useEffect } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 
-import { ACCESS_TOKEN } from '@/constants/auth';
+import { ACCESS_TOKEN, REFRESH_TOKEN } from '@/constants/auth';
 import useAuthStore from '@/stores/auth';
+import { setCookie } from '@/utils/cookie';
 
-// TODO refreshToken 저장 방식 백엔드 팀과 논의 필요
 const OauthRedirect = () => {
   const [searchParams] = useSearchParams();
   const navigate = useNavigate();
   const accessToken = searchParams.get(ACCESS_TOKEN);
-  // const refreshToken = searchParams.get('refreshToken');
+  const refreshToken = searchParams.get(REFRESH_TOKEN);
 
   useEffect(() => {
-    if (accessToken) {
+    if (accessToken && refreshToken) {
       useAuthStore.getState().setAccessToken(accessToken);
+      setCookie({ keyName: REFRESH_TOKEN, value: refreshToken, days: 7 });
       navigate('/');
     }
-  }, [accessToken]);
+  }, [accessToken, navigate, refreshToken]);
 
   return <div>로그인 중</div>;
 };
