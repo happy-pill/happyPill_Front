050ce0f7ab21fb819932c0048f5908864090268a
Choi Jieun <crg1050@gmail.com>
2025-07-13 19:48:43 +0900

    Feat: LayoutContainer prop 추가 및 적용

--- src/components/container/LayoutContainer.tsx
+++ src/components/container/LayoutContainer.tsx
@@ -3,13 +3,25 @@ import { cn } from '@/utils/classNames';
 interface LayoutContainerProps {
   children: React.ReactNode;
   className: string;
-  // isHeader: boolean;
   px?: 'px-md' | 'px-0';
+  isMaxW?: boolean;
+  isHeader?: boolean;
 }
 
-const LayoutContainer = ({ children, className, px = 'px-md' }: LayoutContainerProps) => {
+const LayoutContainer = ({
+  children,
+  className,
+  px = 'px-md',
+  isMaxW = true,
+  isHeader = true,
+}: LayoutContainerProps) => {
   return (
-    <main className={cn(`max-width-container mx-auto mt-[100px] w-full flex-1 ${px}`, className)}>
+    <main
+      className={cn(
+        `mx-auto w-full flex-1 ${isHeader && 'mt-[100px]'} ${isMaxW && 'max-width-container'} ${px}`,
+        className,
+      )}
+    >
       {children}
     </main>
   );
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,10 +1,10 @@
-import LoginContainer from './LoginContainer';
+import LoginContainer from './components/organism/LoginContainer';
 
 import LayoutContainer from '@/components/container/LayoutContainer';
 
 const index = () => {
   return (
-    <LayoutContainer className='flex' px='px-0'>
+    <LayoutContainer className='flex' px='px-0' isMaxW={false}>
       <div className='flex-1 bg-[#F6CDB9] bg-[url("../assets/images/login-img.jpg")] bg-cover bg-center' />
       <LoginContainer />
     </LayoutContainer>
