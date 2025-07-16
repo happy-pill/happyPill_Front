0a5d2240b08850f027c385af7072be286f4dff07
Choi Jieun <crg1050@gmail.com>
2025-07-16 17:07:03 +0900

    Refactor: 불필요 refresh instans 삭제

--- src/apis/instance/refresh.ts
+++ src/apis/instance/refresh.ts
@@ -1,21 +1,6 @@
 import axios, { type AxiosInstance } from 'axios';
 
-import { appStorage } from '@/utils/appStorage';
-
 // 토큰 재발급 인스턴스 생성
 export const RefreshInstance: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
-
-// 요청
-RefreshInstance.interceptors.request.use(
-  function (config) {
-    const currentLanguage = appStorage.getLanguage();
-    config.headers.language = currentLanguage;
-
-    return config;
-  },
-  function (error) {
-    return Promise.reject(error);
-  },
-);
