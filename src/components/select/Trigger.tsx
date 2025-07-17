a14714c1544cda08239c95ee8fd6038f1db59d55
minseung-gang <strause1@nate.com>
2025-07-17 15:28:12 +0900

    feat: Select.Trigger 기능 확장 - icon 요소 외부에서 제어 가능하도록 추가 - icon 위치(left, right) 설정 기능 추가 - icon 숨김 기능 추가 - suffix(단위) 외부 제어 기능 추가

--- src/components/select/Trigger.tsx
+++ src/components/select/Trigger.tsx
@@ -7,18 +7,41 @@ import Button from '../button/BaseButton';
 import { cn } from '@/utils/classNames';
 
 interface SelectTriggerProps {
-  value?: React.ReactNode;
   placeholder?: string;
   className?: string;
+  icon?: React.ReactNode; //아이콘(컴포넌트)
+  iconPosition?: 'left' | 'right'; // 아이콘의 위치
+  hideIcon?: boolean; // 아이콘은 없앨 지
+  suffix?: string; // 단위
 }
 
-const Trigger: React.FC<SelectTriggerProps> = ({ className, placeholder = '선택하세요' }) => {
+const Trigger: React.FC<SelectTriggerProps> = ({
+  className,
+  placeholder = '선택하세요',
+  icon,
+  iconPosition = 'right',
+  hideIcon = false,
+  suffix,
+}) => {
   const { selectedValue, triggerRef, onToggle, isOpen } = useSelectContext();
 
   const handleClick = () => {
     onToggle();
   };
 
+  const renderIcon = () => {
+    if (hideIcon) return null;
+
+    if (icon) {
+      return icon;
+    }
+
+    // 기본 아이콘만 rotate 애니메이션 적용
+    return <IoIosArrowDown className={cn('transition-transform', isOpen && 'rotate-180')} />;
+  };
+
+  const iconElement = renderIcon();
+  const displayValue = selectedValue != null ? `${selectedValue}${suffix || ''}` : placeholder;
   return (
     <Button
       ref={triggerRef}
@@ -28,8 +51,9 @@ const Trigger: React.FC<SelectTriggerProps> = ({ className, placeholder = '선�
         className,
       )}
     >
-      <span>{selectedValue != null ? `${selectedValue}개월` : placeholder}</span>
-      <IoIosArrowDown className={cn(isOpen && 'rotate-180')} />
+      {iconPosition === 'left' && iconElement && renderIcon()}
+      <span>{selectedValue != null ? displayValue : placeholder}</span>
+      {iconPosition === 'right' && iconElement && renderIcon()}
     </Button>
   );
 };
