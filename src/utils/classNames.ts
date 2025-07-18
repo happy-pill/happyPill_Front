425d51663d932fde5891f9e146f66f6073f967d6
minseung-gang <strause1@nate.com>
2025-07-19 00:05:58 +0900

    fix: tailwind-merge 확장하여 커스텀 폰트 사이즈 클래스 충돌 해결

--- src/utils/classNames.ts
+++ src/utils/classNames.ts
@@ -1,6 +1,5 @@
 import { type ClassValue, clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-
+import { extendTailwindMerge } from 'tailwind-merge';
 /**
  * tailwind className 상호간에 merge할 때 발생할 수 있는 충돌을 해결하기 위한 함수입니다.
  * @param  inputs className
@@ -8,5 +7,41 @@ import { twMerge } from 'tailwind-merge';
  */
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return customTwMerge(clsx(inputs));
 }
+
+export function cnWithCustom(...inputs: ClassValue[]) {
+  return clsx(inputs);
+}
+
+// 커스텀 twMerge 함수 생성
+const customTwMerge = extendTailwindMerge({
+  // 커스텀 클래스 그룹 추가
+  extend: {
+    classGroups: {
+      // 폰트 사이즈 관련 커스텀 클래스들
+      'font-size': [
+        'text-xxs-regular',
+        'text-xs-regular',
+        'text-s-regular',
+        'text-s-medium',
+        'text-m-regular',
+        'text-m-medium',
+        'text-l-regular',
+        'text-l-bold',
+        'text-xl-regular',
+        'text-xl-bold',
+        'text-2xl-regular',
+        'text-2xl-bold',
+        'text-3xl-regular',
+        'text-3xl-bold',
+        'text-4xl-regular',
+        'text-4xl-bold',
+        'text-5xl-regular',
+        'text-5xl-bold',
+      ],
+    },
+  },
+});
+
+export default cn;
