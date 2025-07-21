b36cd20264bde7abde5b00fa948db7e1f50f99fd
minseung-gang <strause1@nate.com>
2025-07-21 15:00:57 +0900

    refactor: 전역 LocaleType을 사용하도록 useLocale 수정

--- src/hooks/useLocale.ts
+++ src/hooks/useLocale.ts
@@ -1,15 +1,20 @@
 import { useState } from 'react';
 
+import type { LocaleType } from '@/types/common';
+
 import { appStorage } from '@/utils/appStorage';
 
 const useLocale = () => {
-  const [locale, setLocale] = useState<'ko' | 'en'>(() => appStorage.getLanguage() as 'ko' | 'en');
+  const [locale, setLocale] = useState<LocaleType>(() => ({
+    locale: (appStorage.getLanguage() ?? 'ko') as 'ko' | 'en',
+  }));
 
   const changeLocale = (newLang: 'ko' | 'en') => {
     appStorage.setLanguage(newLang);
-    setLocale(newLang);
+    setLocale({ locale: newLang });
   };
 
   return { locale, changeLocale };
 };
+
 export default useLocale;
