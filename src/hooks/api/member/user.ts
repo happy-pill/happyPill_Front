3e11b21ce16515d4dd29a152a424208ee851a9c0
minseung-gang <strause1@nate.com>
2025-07-21 19:44:07 +0900

    fix: 로그인 상태에 따라 useGetUserInfo 쿼리 활성화 제어

--- src/hooks/api/member/user.ts
+++ src/hooks/api/member/user.ts
@@ -2,11 +2,15 @@ import { useMutation, useQuery } from '@tanstack/react-query';
 
 import userAPI from '@/apis/member/user.api';
 import { queryKey } from '@/constants/queryKey';
+import useLoginedStore from '@/stores/loginedStore';
 
 export const useGetUserInfo = () => {
+  const { isLogined } = useLoginedStore();
   return useQuery({
     queryKey: queryKey.member.user.info,
     queryFn: () => userAPI.getUserInfo(),
+    enabled: !!isLogined,
+    retry: false,
   });
 };
 
