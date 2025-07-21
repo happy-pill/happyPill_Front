2b09667a08e77335b945fe41f27752087acf114a
minseung-gang <strause1@nate.com>
2025-07-21 14:13:17 +0900

    refactor: locale 타입 공통 분리 및 AddToCartModal에 적용

--- src/components/modal/AddToCartModal.tsx
+++ src/components/modal/AddToCartModal.tsx
@@ -6,6 +6,7 @@ import Modal from './ui/Modal';
 import Button from '../button/StyledButton';
 import Select from '../select/Select';
 
+import type { LocaleProps } from '@/types/common';
 import type { ProductItem } from '@/types/products';
 
 import { CART_MODAL } from '@/constants/locale';
@@ -14,7 +15,7 @@ import useModal from '@/hooks/useModal';
 import { cartStorage } from '@/utils/cartStorage';
 
 type CartProductProps = {
-  locale: 'ko' | 'en';
+  locale: LocaleProps;
   product: Omit<ProductItem, 'company' | 'categoryId'>;
 };
 
