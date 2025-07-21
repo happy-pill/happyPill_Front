1204fe557f6513a7d523293f62d67b2e0cf69e0c
minseung-gang <strause1@nate.com>
2025-07-21 17:01:49 +0900

    refactor: CardSkeleton 컴포넌트 리팩토링 - 반복되는 div 태그를 배열과 map으로 간결하게 처리 - 내용 없는 div 태그를 self-closing 태그로 변경하여 코드 가독성 향상

--- src/pages/main/components/atoms/CardSkeleton.tsx
+++ src/pages/main/components/atoms/CardSkeleton.tsx
@@ -3,21 +3,22 @@ const CardSkeleton = () => {
     <div className='rounded-m mb-[12px] animate-pulse overflow-hidden bg-[#ffffffE6] text-left'>
       <div className='relative z-[2] h-[250px]'>
         <div className='absolute top-[20px] left-[20px] flex gap-x-1'>
-          <div className='h-[24px] w-[50px] rounded-full bg-gray-300'></div>
-          <div className='h-[24px] w-[60px] rounded-full bg-gray-300'></div>
+          {Array.from({ length: 2 }).map((_, idx) => (
+            <div key={idx} className='h-[24px] w-[50px] rounded-full bg-gray-300' />
+          ))}
         </div>
 
-        <div className='h-full w-full bg-gray-300'></div>
+        <div className='h-full w-full bg-gray-300' />
       </div>
 
       <div className='px-[20px] py-[30px]'>
-        <div className='mb-[15px] h-[24px] w-[70%] rounded bg-gray-300'></div>
+        <div className='mb-[15px] h-[24px] w-[70%] rounded bg-gray-300' />
 
-        <div className='mb-[20px] h-[12px] w-[40%] rounded bg-gray-300'></div>
+        <div className='mb-[20px] h-[12px] w-[40%] rounded bg-gray-300' />
 
         <div className='flex items-center justify-between'>
-          <div className='h-[20px] w-[80px] rounded bg-gray-300'></div>
-          <div className='h-[36px] w-[36px] rounded-full bg-gray-300'></div>
+          <div className='h-[20px] w-[80px] rounded bg-gray-300' />
+          <div className='h-[36px] w-[36px] rounded-full bg-gray-300' />
         </div>
       </div>
     </div>
