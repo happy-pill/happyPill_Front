8a9afc693b722d92cda2e7fb6bb5e2c271546bed
minseung-gang <strause1@nate.com>
2025-07-22 10:15:20 +0900

    chore: useGetUserInfo fetch 조건 제거 및 staleTime 설정

--- src/hooks/api/member/user.ts
+++ src/hooks/api/member/user.ts
@@ -2,15 +2,13 @@ import { useMutation, useQuery } from '@tanstack/react-query';
 
 import userAPI from '@/apis/member/user.api';
 import { queryKey } from '@/constants/queryKey';
-import useLoginedStore from '@/stores/loginedStore';
 
 export const useGetUserInfo = () => {
-  const { isLogined } = useLoginedStore();
   return useQuery({
     queryKey: queryKey.member.user.info,
     queryFn: () => userAPI.getUserInfo(),
-    enabled: !!isLogined,
     retry: false,
+    staleTime: 5 * 60 * 1000,
   });
 };
 
