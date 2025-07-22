1844fb838f6a612d894b6350d282916570053874
minseung-gang <strause1@nate.com>
2025-07-22 10:06:03 +0900

    feat: Layout에 로그인 상태 초기화 및 온보딩 리디렉션 훅 적용

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,8 +4,11 @@ import Footer from './Footer';
 import Header from './Header';
 import GlobalContainer from './modal/GlobalContainer';
 
+import { useAuthStateManager } from '@/hooks/useAuthStateManager';
+
 // TODO 각 페이지 확인 후 LayoutContainer 추가 여부 관련 수정 필요
 const Layout = () => {
+  useAuthStateManager();
   return (
     <div className='flex min-h-screen flex-col'>
       <Header />
