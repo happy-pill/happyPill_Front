04cc0d77f136d24793a974e9dae532f847770dda
minseung-gang <strause1@nate.com>
2025-07-25 14:55:04 +0900

    feat: ProductPage 컴포넌트에 필요한 상수 분리 및 정리

--- src/constants/locale.ts
+++ src/constants/locale.ts
@@ -1,3 +1,10 @@
+// common
+
+export const CURRENCY_UNIT = {
+  ko: '원',
+  en: 'KRW',
+};
+
 // main page
 
 export const MAIN_BANNER_DATA = [
@@ -96,6 +103,8 @@ export const PROMOTION_BANNER_DATA = [
   },
 ];
 
+// modal
+
 export const CREATE_WELCOME_STEPS = [
   {
     ko: {
@@ -166,5 +175,40 @@ export const CART_COMPLETE_MODAL = {
   },
   en: {
     message: 'Your item has been added to the cart.',
+  },
+};
+
+// product page
+
+export const PRODUCT_DELIVERY_BADGE = {
+  ko: {
+    prefix: '이 상품은',
+    highlight: '내일도착, 무료배송',
+  },
+  en: {
+    prefix: 'This product qualifies for',
+    highlight: 'Next-day Delivery & Free Shipping',
+  },
+};
+
+export const RELATED_PRODUCTS_SECTION = {
+  ko: {
+    title: '다른 고객이 함께 본 상품',
+    currencyUnit: '원',
+  },
+  en: {
+    title: 'Customers Also Viewed',
+    currencyUnit: 'KRW',
+  },
+};
+
+export const PRODUCT_TABS = {
+  ko: {
+    'product-info': '상세정보',
+    'product-detail': '제품상세',
+  },
+  en: {
+    'product-info': 'Product Info',
+    'product-detail': 'Details',
   },
 };
