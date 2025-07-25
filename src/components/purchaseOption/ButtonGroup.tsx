b1bc878a5820d08da1db23134f436e4e6f3a3a39
minseung-gang <strause1@nate.com>
2025-07-25 18:30:03 +0900

    feat: PurchaseOption 컴포넌트에 ButtonGroup 하위 컴포넌트 구현

---
+++ src/components/purchaseOption/ButtonGroup.tsx
@@ -0,0 +1,10 @@
+interface ButtonGroupProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const ButtonGroup = ({ children, className = '' }: ButtonGroupProps) => {
+  return <div className={`mt-5 flex justify-center gap-x-3 ${className}`}>{children}</div>;
+};
+
+export default ButtonGroup;
