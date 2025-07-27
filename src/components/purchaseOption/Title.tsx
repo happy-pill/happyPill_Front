205c0d0aefa5ad3c580cb5e103a5bca73b65a720
minseung-gang <strause1@nate.com>
2025-07-27 21:28:42 +0900

    refactor: Title 컴포넌트에서 불필요한 ClassName 초기값 제거

---
+++ src/components/purchaseOption/Title.tsx
@@ -0,0 +1,10 @@
+interface TitleProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Title = ({ children, className }: TitleProps) => {
+  return <h2 className={`text-m-medium mb-2.5 ${className}`}>{children}</h2>;
+};
+
+export default Title;
