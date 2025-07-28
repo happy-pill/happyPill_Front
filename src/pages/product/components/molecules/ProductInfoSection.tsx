b91cc3a37d963efb9f28c6b26ba92af4c36add67
minseung-gang <strause1@nate.com>
2025-07-28 13:08:15 +0900

    refactor: alt 속성에서 불필요한 템플릿 리터럴 제거

--- src/pages/product/components/molecules/ProductInfoSection.tsx
+++ src/pages/product/components/molecules/ProductInfoSection.tsx
@@ -52,7 +52,7 @@ const ProductInfoSection: React.FC<productInfoProps> = ({
 
   return (
     <div className='max-width-container mx-auto grid w-full grid-cols-1 gap-x-5 md:grid-cols-2'>
-      <ProductThumbnail src={product?.thumbnailUrl} alt={`${product?.name}`} />
+      <ProductThumbnail src={product?.thumbnailUrl} alt={product?.name} />
       <div className='pt-5 md:p-8'>
         <ProductHeader product={product} />
 
