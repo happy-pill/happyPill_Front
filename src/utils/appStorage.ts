1ae7947187f8c83f9cc5be1a05cd39ba56618a79
minseung-gang <strause1@nate.com>
2025-07-30 23:49:36 +0900

    refactor: 다국어 관련 스토리지 키를 상수로 분리

--- src/utils/appStorage.ts
+++ src/utils/appStorage.ts
@@ -1,8 +1,10 @@
+import { APP_LANGUAGE_STORAGE_KEY } from '@/constants/common';
+
 /**
  * getLanguage : APP_LANGUAGE 데이터 가져오기
  * setLanguage : APP_LANGUAGE 스토리지에 저장
  */
 export const appStorage = {
-  getLanguage: () => localStorage.getItem('APP_LANGUAGE'),
-  setLanguage: (lang: string) => localStorage.setItem('APP_LANGUAGE', lang),
+  getLanguage: () => localStorage.getItem(APP_LANGUAGE_STORAGE_KEY),
+  setLanguage: (lang: string) => localStorage.setItem(APP_LANGUAGE_STORAGE_KEY, lang),
 };
