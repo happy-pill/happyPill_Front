30fc079f06620b3f1ac0c715520ddfaaadc6ecf1
minseung-gang <strause1@nate.com>
2025-07-30 19:50:36 +0900

    feat: 상품 카드에 상품 정보 표시 컴포넌트 구현

---
+++ src/pages/cart/components/atoms/ProductInfo.tsx
@@ -0,0 +1,16 @@
+interface ProductInfoProps {
+  productName: string;
+  briefDescription: string;
+}
+const ProductInfo = ({ productName, briefDescription }: ProductInfoProps) => {
+  return (
+    <div>
+      <p className='text-[clamp(11px,1vw,16px)] font-bold'>{productName}</p>
+      <p className='mt-[clamp(5px,1vw,7px)] line-clamp-2 text-[clamp(10px,1vw,13px)] leading-[clamp(12px,1vw,17px)] text-[#666666]'>
+        {briefDescription}
+      </p>
+    </div>
+  );
+};
+
+export default ProductInfo;
