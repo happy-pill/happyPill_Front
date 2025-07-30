e9c5d603cfdd68b97e723a5317eb6ac422785bed
minseung-gang <strause1@nate.com>
2025-07-30 22:10:37 +0900

    refactor: tailwind 유틸 및 커스텀 클래스 중복제거를 위해 cn함수 적용

--- src/components/purchaseOption/Title.tsx
+++ src/components/purchaseOption/Title.tsx
@@ -1,10 +1,12 @@
+import cn from '@/utils/classNames';
+
 interface TitleProps {
   children: React.ReactNode;
   className?: string;
 }
 
 const Title = ({ children, className }: TitleProps) => {
-  return <h2 className={`mb-2.5 ${className}`}>{children}</h2>;
+  return <h2 className={cn(`mb-2.5 ${className}`)}>{children}</h2>;
 };
 
 export default Title;
