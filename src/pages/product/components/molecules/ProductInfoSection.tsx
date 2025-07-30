91bd5baed90562dd2920181921e85de009b44936
minseung-gang <strause1@nate.com>
2025-07-30 22:36:45 +0900

    style: 불필요한 max-width 스타일 제거

--- src/pages/product/components/molecules/ProductInfoSection.tsx
+++ src/pages/product/components/molecules/ProductInfoSection.tsx
@@ -51,7 +51,7 @@ const ProductInfoSection: React.FC<productInfoProps> = ({
   ] as const;
 
   return (
-    <div className='max-width-container mx-auto grid w-full grid-cols-1 gap-x-5 md:grid-cols-2'>
+    <div className='grid w-full grid-cols-1 gap-x-5 md:grid-cols-2'>
       <ProductThumbnail src={product?.thumbnailUrl} alt={product?.name} />
       <div className='pt-5 md:p-8'>
         <ProductHeader product={product} />
