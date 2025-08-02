63742e8f882a31f10e4e489f6cfb3ab82dad6490
Choi Jieun <crg1050@gmail.com>
2025-08-03 00:03:39 +0900

    Feat/Refactor: admin navigation 상수로 관리 및 스타일 조건문 수정

---
+++ src/constants/navigation.ts
@@ -0,0 +1,24 @@
+import { routePath } from './path';
+
+export const ADMIN_NAV_ITEMS = [
+  {
+    type: 'managementSubscribe',
+    name: '구독 상품 관리',
+    path: routePath.admin.management.subscribe,
+  },
+  {
+    type: 'managementUser',
+    name: '회원 관리',
+    path: routePath.admin.management.user.root,
+  },
+  {
+    type: 'managementProduct',
+    name: '상품 관리',
+    path: routePath.admin.management.product,
+  },
+  {
+    type: 'managementCategory',
+    name: '카테고리 관리',
+    path: routePath.admin.management.category,
+  },
+];
