7744d4cac8bc41bf413fbf50f2a277b9453852ec
minseung-gang <strause1@nate.com>
2025-08-05 20:24:43 +0900

    refactor: OrderSumaary 컴포넌트 리팩토링 - a태그를 Link 태그로 수정 - Accordion 컴포넌트 경로 수정

--- src/pages/purchase/components/organisms/OrderSummary.tsx
+++ src/pages/purchase/components/organisms/OrderSummary.tsx
@@ -6,7 +6,7 @@ import type { CheckoutProduct } from '@/types/products';
 import { LOCALE_LABELS } from '@/constants/locale/purchase';
 import { routePath } from '@/constants/path';
 import useLocale from '@/hooks/useLocale';
-import AccordionSection from '@/pages/purchase/components/atoms/AccordionSection';
+import Accordion from '@/pages/purchase/components/molecules/Accordion';
 import { cn } from '@/utils/classNames';
 
 interface OrderSummaryProps {
@@ -18,7 +18,7 @@ const OrderSummary: React.FC<OrderSummaryProps> = ({ items }) => {
   const ORDER_SUMMARY = LOCALE_LABELS[locale].orderSummary;
   return (
     <div className='rounded-md bg-white'>
-      <AccordionSection title={ORDER_SUMMARY.title}>
+      <Accordion title={ORDER_SUMMARY.title}>
         {items &&
           items.map((item, idx) => {
             const isLast = idx === items.length - 1;
@@ -27,9 +27,9 @@ const OrderSummary: React.FC<OrderSummaryProps> = ({ items }) => {
               <div key={`${item.productName} + ${idx}`} className={cn('p-5', idx !== 0 && 'pt-3')}>
                 <div className={cn(`flex border-b border-[#e9e9e9] py-5`, isLast && 'border-none')}>
                   <div className='mr-5 h-[90px] w-[90px]'>
-                    <a href={routePath.common.product.route(item.productId)}>
+                    <Link to={routePath.common.product.route(item.productId)}>
                       <img src={item.thumbnailUrl} alt={item.productName} width={90} height={90} />
-                    </a>
+                    </Link>
                   </div>
                   <div className='grid'>
                     <Link to={routePath.common.product.route(item.productId)}>
@@ -50,7 +50,7 @@ const OrderSummary: React.FC<OrderSummaryProps> = ({ items }) => {
               </div>
             );
           })}
-      </AccordionSection>
+      </Accordion>
     </div>
   );
 };
