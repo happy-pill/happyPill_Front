d805e0891dc1ea4f7d6ce54713232806caeb47eb
Choi Jieun <crg1050@gmail.com>
2025-08-13 03:48:31 +0900

    Feat: input textState 추가 및 props 변경

--- src/components/input/FiledInput.tsx
+++ src/components/input/FiledInput.tsx
@@ -4,6 +4,7 @@ import StyledInput from './StyledInput';
 import InputErrorMsg from './ui/InputErrorMsg';
 import InputIcon from './ui/InputIcon';
 import InputLabel from './ui/InputLabel';
+import InputTextState from './ui/InputTextState';
 
 import type { UseFormRegister, FieldValues } from 'react-hook-form';
 
@@ -16,19 +17,29 @@ interface FiledInputProps extends InputHTMLAttributes<HTMLInputElement> {
   errorMsg?: string;
   iconItem?: { icon: React.ReactNode; position: 'left' | 'right' };
   contentItem?: { item: React.ReactNode; position: 'left' | 'right' };
+  textState?: string | number;
   register?: UseFormRegister<FieldValues>;
 }
 
 const FiledInput = forwardRef<HTMLInputElement, FiledInputProps>(
-  ({ className, label, isRequired, errorMsg, iconItem, contentItem, register, ...rest }, ref) => {
+  (
+    { className, label, isRequired, errorMsg, iconItem, contentItem, textState, register, ...rest },
+    ref,
+  ) => {
     return (
       <label className='flex w-full flex-col items-start gap-1'>
-        {label && (
-          <InputLabel>
-            {label}
-            {isRequired && <span className='text-invalid'>*</span>}
-          </InputLabel>
-        )}
+        <div className='flex w-full items-center justify-between'>
+          {label && (
+            <InputLabel>
+              {label}
+              {isRequired && <span className='text-invalid'>*</span>}
+            </InputLabel>
+          )}
+
+          {textState && rest.maxLength && (
+            <InputTextState>{`${textState} / ${rest.maxLength}`}</InputTextState>
+          )}
+        </div>
 
         <div className='relative w-full'>
           {iconItem && (
