7387d733c9fb528d40d4b985dccf17d8f8d1f638
Choi Jieun <crg1050@gmail.com>
2025-08-13 03:59:40 +0900

    Fix: admin layout height 수정 및 불필요한 주석 삭제

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -6,7 +6,6 @@ import GlobalContainer from '../modal/GlobalContainer';
 
 import { useAuthStateManager } from '@/hooks/useAuthStateManager';
 
-// TODO 각 페이지 확인 후 LayoutContainer 추가 여부 관련 수정 필요
 const Layout = () => {
   useAuthStateManager();
   return (
